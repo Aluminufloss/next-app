20e74f581e0114d5dbcbb94820fb582584c713b9
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-10-05 21:11:55 +0300

    Add global constants file with charset variants (#5)

---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const SYMBOLS = '%*?)@#$~';
+export const NUMBERS = '0123456789';
+export const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz';
+export const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
