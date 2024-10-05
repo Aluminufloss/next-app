151759260f1e1e38780a52f0ff247f5cf4e21533
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-10-06 00:04:48 +0300

    Add title and update header logo (#5)

--- src/components/Header/Logo/index.tsx
+++ src/components/Header/Logo/index.tsx
@@ -3,9 +3,19 @@
 import Image from "next/image";
 
 import logo from "@/assets/logo.png";
+import Link from "next/link";
 
 const Logo: React.FC = () => {
-  return <Image src={logo} alt="logo" width={142} height={24} />;
+  return (
+    <Link href={"/"}>
+      <Image 
+        src={logo} 
+        alt="Logo" 
+        width={142} 
+        height={24} 
+      />
+    </Link>
+  )
 };
 
 export default Logo;
